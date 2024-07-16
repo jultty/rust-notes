acc682c1bf38b98471cc3009c5ecc41044347c74
jultty <juno.t@tuta.io>
2024-07-16 15:26:50 -0300

    The Book 2.0: Up to "Generating a Random Number"

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+fn guess() {
+    use std::io;
+    use rand::Rng;
+    use std::cmp::Ordering;
+
+    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Guess the number!\nYour guess: ");
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    guess()
 }
